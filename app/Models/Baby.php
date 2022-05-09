e0532b4fb3f09f68fba8b5221ef54c880c87b01e
Ilham-sinatrio20 <sinatrio20@gmail.com>
2022-05-09 14:53:56 +0700

    Starter Model & Migration

---
+++ app/Models/Baby.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Baby extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'nama',
+        'id_parent',
+        'tempat_lahir',
+        'tanggal_lahir',
+        'anak_ke',
+        'jenis_kelamin',
+        'golongan_darah',
+        'panjang_bayi',
+        'berat_bayi',
+    ];
+
+    public function progressBaby(){
+        return $this->hasMany(ProgressBaby::class);
+    }
+
+    public function parent() {
+        return $this->belongsTo(Parents::class);
+    }
+
+    public function immunization() {
+        return $this->hasMany(Immunization::class);
+    }
+}
---
+++ database/migrations/2022_05_09_073423_create_progress_babies_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProgressBabiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('progress_babies', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_bayi')->nullable();
+            $table->integer('bulan_ke');
+            $table->decimal('panjang_bayi', 11, 2);
+            $table->decimal('berat_bayi', 11, 2);
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('id_bayi')->references('id')->on('babies');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('progress_babies');
+    }
+}
